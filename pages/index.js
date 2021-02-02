6ad266cd1b7178adb9764c6d3bf635d4ef7ca9d9
eric-rafael <44949879+eric-rafael@users.noreply.github.com>
2021-02-02 12:02:01 -0300

    add msn de obg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <div>Home teste</div>
+    return <div>Home de Testes: Obrigado por acessar o site de Eric Rafael, seu acesso me ajuda bastante!</div>
 }
 
 export default Home
