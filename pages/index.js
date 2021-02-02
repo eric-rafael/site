f137587362b6900999caaefb6e7187e45e2ff037
eduardo.ontecnologia <eduardo.ontecnologia@ccp.caedu>
2021-02-02 10:34:02 -0300

    componentsCriados

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,3 @@ function Home(){
 }
 
 export default Home
-
-<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-  Open Menu
-</Button>
-<Menu
-  id="simple-menu"
-  anchorEl={anchorEl}
-  keepMounted
-  open={Boolean(anchorEl)}
-  onClose={handleClose}
->
-  <MenuItem onClick={handleClose}>Profile</MenuItem>
-  <MenuItem onClick={handleClose}>My account</MenuItem>
-  <MenuItem onClick={handleClose}>Logout</MenuItem>
-</Menu>
-
-export default Menu
